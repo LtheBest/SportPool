08bed3b6598c89efaea250cca9cd6ef65b9a0d4a
LOICT <loict@LOICT.local>
2025-09-02 20:00:40 +0200

    chore(git): update routes and index

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes-jwt"; // Switch to JWT routes
+import { registerRoutes } from "./routes"; // Switch to JWT routes
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
